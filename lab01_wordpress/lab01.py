3f84fe21b5674b3ead55e1652db9b4ac09ba98b4
Alejandro-Rojas <rojas.alejandro303@gmail.com>
2021-07-07 19:19:06 -0600

    update

--- lab01_wordpress/lab01.py
+++ lab01_wordpress/lab01.py
@@ -1,16 +1,13 @@
-# CS39AB - Cloud Computing - Summer 2021 
-# Instructor: Thyago Mota
-# Description: lab01
-# Members: Alejandro Rojas 
+# Lab01 CS39AB
+# Alejandro Rojas
+# IP 54.193.58.116
+# http://54.193.58.116/2021/07/07/my-first-aws-blog/
 
 import boto3
 import time
 
-# us-west-1(N.California)
-
-
 if __name__ == "__main__":
-    # Step 1: obtain the id of the default VPC
+   # TODOd: obtain the id of the default VPC
     client = boto3.client('ec2')
     vpcs = client.describe_vpcs()['Vpcs']
     default_vpc_id = None
@@ -24,8 +21,7 @@ if __name__ == "__main__":
     print('Default VPC id is ' + default_vpc_id)
 
 
-    # Step 2: obtain the id of the subnet that has the 
-        # property MapPublicIpOnLaunch(Public Subnet) set to true
+   # TODOd: obtain the id of the subnet that has the property MapPublicIpOnLaunch set to true
     filter = {
         'Name': 'vpc-id',
         'Values': [ default_vpc_id ]
@@ -42,18 +38,14 @@ if __name__ == "__main__":
     print('Public subnet id is ' + public_subnet_id)
 
 
-    # Step 3: Create a security group named lab01
+        # TODOd: create a security group 
     sg_id = client.create_security_group(
         GroupName = 'lab01',
         Description = 'lab01',
         VpcId = default_vpc_id)['GroupId']
     print('Security group was created with id ' + sg_id)
 
-
-    # Step 4: add an ingress rule to security group
-        # add with inbound ssh (port 22), http (port 80), 
-        # and mysql/mariadb (port 3306) traffic enabled from anywhere.
-    # port 22
+ # TODOd: add an ingress rule to security group
     ip_permission_ssh = {
         'FromPort': 22,
         'ToPort':   22,
@@ -97,7 +89,7 @@ if __name__ == "__main__":
         IpPermissions = [ ip_permission_ssh, ip_permission_http, ip_permission_mysql ]
     )   
 
-    # Step 5: Launch ec2 instance 
+    # TODOd: launch ec2 instance 
     user_data = '''#!/bin/bash
 sudo yum update -y
 sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
@@ -110,7 +102,7 @@ wget https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
 cp wordpress/wp-config-sample.php wordpress/wp-config.php'''
     instance_id = client.run_instances(
-        ImageId = 'ami-0b2ca94b5b49e0132',
+        ImageId = 'ami-02f24ad9a1d24a799',
         MinCount = 1, 
         MaxCount = 1,
         InstanceType = 't2.micro',
@@ -120,10 +112,10 @@ cp wordpress/wp-config-sample.php wordpress/wp-config.php'''
         UserData = user_data
     )['Instances'][0]['InstanceId']
     print('Instance id is ' + instance_id)
-
-    # Display the public IP of the EC2 instance before it finishes. (no wait time)
+ # wait for 30 minute for instance to be launched
     print('Waiting for instance to be launched...')
     time.sleep(30)
+# TODOd: get the public IP of your instance
     ip_address = client.describe_instances(
         InstanceIds = [ instance_id ]
     )['Reservations'][0]['Instances'][0]['PublicIpAddress']
